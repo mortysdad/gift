78d37e1e0ae1a063886f5617330965451957f310
Rahul Soni <rahulsoni2225@gmail.com>
2018-07-29 13:59:58 +0530

    deleted navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,11 @@
 <template>
   <div>
-    <Navbar></Navbar>
+    
     <nuxt/>
   </div>
 </template>
 
-<script>
-  import Navbar from '../components/Navbar'
-  export default {
-    components: {
-      Navbar
-    }
-  }
-</script>
+
 
 <style>
 html
